964afcfe55dde67a5b4d24f1d2104898d20b5b39
Diego J Perez <diegojperez90@gmail.com>
2022-04-04 22:03:52 -0300

    realizando Promise

--- Javascript/objetos.js
+++ Javascript/objetos.js
@@ -1,31 +1,8 @@
-class Producto {
-    constructor(id, imagen, nombre, precioLista, stock) {
-        this.id = id,
-        this.imagen = imagen,
-        this.nombre = nombre,
-        this.precioLista = precioLista,
-        this.stock = stock
-    }
-}
-
-let producto1 = new Producto(1, './imagenes/maletin.jpg','Maletin de poker', 6000, 10);
-let producto2 = new Producto(2, './imagenes/backgammon.webp','Backgammon', 3000, 5);
-let producto3 = new Producto(3, './imagenes/ruleta.jpeg','Ruleta', 4500, 20);
-let producto4 = new Producto(4, './imagenes/ajedrez.jpg','Ajedrez PRO', 7800, 3);
-let producto5 = new Producto(5, './imagenes/monopoly.jpeg','Monopoly', 4200, 4);
-let producto6 = new Producto(6, './imagenes/teg.png','Teg', 5500, 5);
-let producto7 = new Producto(7, './imagenes/carrera-mente.jpeg','Carrera de mente', 14000, 1);
-let producto8 = new Producto(8, './imagenes/pictionary.webp','Pictionary', 4500, 8);
-
-
-const juegos = [ producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8];
-
 const carritoCompras = [];
-
 
 const contenedorMain = document.querySelector('.contenedorMain')
 
-const crearTarjetaProductos = () => {
+const crearTarjetaProductos = (juegos) => {
     juegos.forEach ( (elem) => {
         const columna = document.createElement('div');
         const tarjeta = document.createElement('div');
@@ -47,12 +24,18 @@ const crearTarjetaProductos = () => {
         botonAgregar.onclick = () => {
             elem.idTemp = new Date().getTime();
             
-            //Operador SPREAD 
-            const nuevoProductos =  {...elem};
+            const nuevoProducto =  {...elem};
+            console.log(carritoCompras);
+
+            carritoCompras.push(nuevoProducto);
+            localStorage.setItem('carrito_de_compras',JSON.stringify(carritoCompras));
+            // if (elem.stock != 0) {
+            //     elem.stock -= 1;
+            // }
             
-            carritoCompras.push(nuevoProductos);
-            localStorage.setItem('carrito_de_compras',JSON.stringify( carritoCompras) );
-           //Aplicando libreria TOASTIFY
+            // console.log(elem.stock)
+
+        
             Toastify({
                 text: "Agregaste " + elem.nombre + " al carrito",
                 position: 'center',
@@ -82,11 +65,13 @@ const crearTarjetaProductos = () => {
 }
 
 
-crearTarjetaProductos();
+fetch('../data/data.json')
+    .then(respuesta => respuesta.json())
+    .then (datos => {
+        crearTarjetaProductos(datos)
+})
 
 
-
-
-
+actualizandoCarrito();
 
 
